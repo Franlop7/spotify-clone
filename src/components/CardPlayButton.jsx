2775e28c272df34c7c0447276ff7394727db70c5
Fran Lopez <franciscolopto@gmail.com>
2023-10-17 19:23:27 +0200

    add cardplaybutton component

---
+++ src/components/CardPlayButton.jsx
@@ -0,0 +1,27 @@
+import { Play, Pause } from "./Player";
+import { usePlayerStore } from "./store/playerStore";
+
+export function CardPlayButton({ id }) {
+  const { currentMusic, isPlaying, setIsPlaying, setCurrentMusic } =
+    usePlayerStore((state) => state);
+
+  const handleClick = () => {
+    setCurrentMusic({
+      playlist: {
+        id,
+      },
+    });
+    setIsPlaying(!isPlaying);
+  };
+
+  const isPlayingPlaylist = isPlaying && currentMusic?.playlist.id === id;
+
+  return (
+    <button
+      onClick={handleClick}
+      className="card-play-button rounded-full bg-green-500 p-4"
+    >
+      {isPlayingPlaylist ? <Pause /> : <Play />}
+    </button>
+  );
+}
