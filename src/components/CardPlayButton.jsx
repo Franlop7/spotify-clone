7c058164fe5607a825fdd135f91f85291191b3b1
Fran Lopez <franciscolopto@gmail.com>
2023-10-18 21:14:52 +0200

    add handleclick

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -1,20 +1,25 @@
 import { Play, Pause } from "./Player";
-import { usePlayerStore } from "./store/playerStore";
+import { usePlayerStore } from "@/store/playerStore";
 
 export function CardPlayButton({ id }) {
   const { currentMusic, isPlaying, setIsPlaying, setCurrentMusic } =
     usePlayerStore((state) => state);
 
-  const handleClick = () => {
-    setCurrentMusic({
-      playlist: {
-        id,
-      },
-    });
-    setIsPlaying(!isPlaying);
-  };
-
   const isPlayingPlaylist = isPlaying && currentMusic?.playlist.id === id;
+
+  const handleClick = () => {
+    if (isPlayingPlaylist) {
+      setIsPlaying(false);
+      return;
+    }
+    fetch(`/api/get-info-playlist.json?id=${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        const { songs, playlist } = data;
+        setIsPlaying(true);
+        setCurrentMusic({ songs, playlist, song: songs[0] });
+      });
+  };
 
   return (
     <button
